595f3556c7e1429faf495e671dc73a7173193d94
stefansaveski <stefansaveski19@gmail.com>
2024-02-10 00:36:40 +0100

    working on

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Leaderboard from "./Leaderboard";
 import './app.css'
+import Siteupdates from "./Siteupdates";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
               <Leaderboard></Leaderboard>
             </div>
           </div>
+          <Siteupdates></Siteupdates>
         </Container>
       </React.Fragment>
     </>
---
+++ react-frontend/src/Siteupdates.jsx
@@ -0,0 +1,15 @@
+import Updatecard from './updatecard';
+import './updates.css'
+
+
+function Siteupdates() {
+  return (
+    <>
+      <div className="updates-title">Recent site updates</div>
+      <div><Updatecard></Updatecard></div>
+      
+    </>
+  );
+}
+
+export default Siteupdates;
