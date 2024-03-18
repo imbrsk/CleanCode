f00ee9a87295ac25c85bcb8ecb4940c8e4f20350
imbrsk <boris696boris@gmail.com>
2024-03-18 21:30:09 +0100

    added problem loading and code exec

--- RustRocket/src/runcode.rs
+++ RustRocket/src/runcode.rs
@@ -2,7 +2,9 @@ use std::fmt::format;
 use reqwest::{self, dns::Resolve};
 use rocket::{response, serde::json::Json, FromForm, State};
 use serde::Deserialize;
-use serde_json::json;
+use serde_json::from_str;
+use serde_json::{json, to_value};
+use sqlx::Row;
 
 enum ProcessCodeResponse{
     Success,
@@ -11,10 +13,10 @@ enum ProcessCodeResponse{
 #[derive(Debug, FromForm, Deserialize)]
 pub struct ProblemData{
     code: String,
-    language: i8,
-    user: String,
-    subject: String,
-    problem: String,
+    language: String,
+    session: String,
+    path: String,
+    problem_id: String,
 }
 impl ProblemData{
     async fn make_api_req(payload: serde_json::Value)->Result<Json<serde_json::Value>, reqwest::Error> {
@@ -43,14 +45,32 @@ impl ProblemData{
             return ProcessCodeResponse::Success;
         }
     }
-    async fn get_input_expected(&self, field : String , pool: &State<sqlx::MySqlPool>) -> Json<serde_json::Value>{
-        let sent_data = format!("SELECT {} FROM subjects WHERE problem_name = ?", field);
-        let correct = sqlx::query_as::<_,   (serde_json::Value,)>(&sent_data)
-            .bind(self.problem.clone())
+    async fn get_input_expected(&self, field : String , pool: &State<sqlx::MySqlPool>) -> String{
+        let sent_data = format!("SELECT {} FROM subjects WHERE id = ? AND path = ?", field);
+        let correct:(String, ) = sqlx::query_as(&sent_data)
+            .bind(self.problem_id.clone())
+            .bind(self.path.clone())    
             .fetch_one(&**pool)
             .await
             .unwrap();
-        rocket::serde::json::Json(correct.0)
+        correct.0
+    }
+    async fn get_user_id(&self, pool: &State<sqlx::MySqlPool>) -> sqlx::mysql::MySqlRow{
+        let user_id = sqlx::query("SELECT user_id FROM sessions WHERE session_id = ?")
+            .bind(self.session.clone())
+            .fetch_one(&**pool)
+            .await
+            .unwrap();
+        user_id
+    }
+    async fn save_code(&self ,user_id: i32, pool: &State<sqlx::MySqlPool>){
+        sqlx::query("INSERT INTO solved (user_id, problem_id, code) VALUES (?, ?, ?)")
+            .bind(user_id.to_string())
+            .bind(self.problem_id.clone())
+            .bind(self.code.clone())
+            .execute(&**pool)
+            .await
+            .unwrap();
     }
     pub async fn make_code_req(&self,  pool: &State<sqlx::MySqlPool>)-> Json<serde_json::Value>{
         let mut payload = json!({
@@ -71,10 +91,22 @@ impl ProblemData{
                     let mut submission_data = json!({});
                     let mut cor_data = json!({});
                     let mut track_cor:i8 = 0;
+                    let input_value: Result<serde_json::Value, _> = serde_json::from_str(&input);
+                    let expected_value: Result<serde_json::Value, _> = serde_json::from_str(&expected);
                     for i in 0i8..10 {
                         let temp = format!("test{}", i);
-                        payload["stdin"] = input[&temp].clone();
-                        payload["expected_output"] = expected[&temp].clone();
+                        if let Ok(ref value) = input_value {
+                            let test = value.get(&temp);
+                            if let Some(test_str) = test.as_ref().and_then(|v| v.as_str()) {
+                                payload["stdin"] = test_str.into();
+                            }
+                        }   
+                        if let Ok(ref value1) = expected_value {
+                            let test = value1.get(&temp);
+                            if let Some(test_str) = test.as_ref().and_then(|v| v.as_str()) {
+                                payload["expected_output"] = test_str.into();
+                            }
+                        } 
                         let response = ProblemData::make_api_req(payload.clone()).await.unwrap().0;
                         submission_data[&temp] = response["stdout"].clone();
                         if response["status"]["description"] == "Wrong Answer" {
@@ -84,14 +116,18 @@ impl ProblemData{
                             cor_data[temp] = "1".into();
                         }
                     }
-                    drop(payload);
-                    return Json(json!({
-                "inpit": *input,
-                "expected": *expected,
-                "got": submission_data,
-                "is_cor": cor_data,
-                "track_cor": track_cor
-                })) } 
+                        if track_cor == 10{
+                            let user_id = self.get_user_id(pool).await;
+                            self.save_code(user_id.get("user_id"), pool).await;
+                        }
+                        drop(payload);
+                        return Json(json!({
+                    "input": input_value.unwrap(),
+                    "expected": expected_value.unwrap(),
+                    "got": submission_data,
+                    "is_cor": cor_data,
+                    "track_cor": track_cor
+                    })) } 
         };
     }
 }
