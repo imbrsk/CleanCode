786148a47963fe494e99dfcef1ebbfd603a933e1
stefansaveski <stefansaveski19@gmail.com>
2024-03-19 14:25:00 +0100

    fixed accordion

--- react-frontend/src/components/Accordion.jsx
+++ react-frontend/src/components/Accordion.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../css/problems.css";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function Accordion(props) {
   const activeIndex = props.active;
