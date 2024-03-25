d3273fac7b63accea28e96bee047d2b24fb0efb4
imbrsk <boris696boris@gmail.com>
2024-03-25 11:46:38 +0100

    fixed mapping issue with leaderboard get req

--- react-frontend/src/components/Leaderboard.jsx
+++ react-frontend/src/components/Leaderboard.jsx
@@ -1,57 +1,41 @@
 import "../css/leaderboard.css";
 import Rankeduser from "./Rankeduser";
 import * as React from "react";
+
 function Leaderboard() {
-  let name = "CodeMaster";
-  let soltask = 454;
-let rankedusers = [
-    {
-            rank: "1",
-            name: "Stefan",
-            tasks: "123"
-    },
-    {
-            rank: "2",
-            name: "Boris",
-            tasks: "12"           
-    },
-    {
-            rank: "3",
-            name: "Petar",
-            tasks: "423"
-    }
-]
-React.useEffect(() => {
+  const [rankedusers, setRankedusers] = React.useState([]);
+
+  React.useEffect(() => {
     fetchLB();
   }, []);
-const fetchLB = async () => {
+
+  const fetchLB = async () => {
     try {
       const response = await fetch("http://localhost:8000/leaderboard");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      rankedusers = await response.json();
-      // Sort subjects by their number
-      data.sort((a, b) => a.number - b.number);
-      setSubjects(data);
+      const data = await response.json();
+      setRankedusers(data);
     } catch (error) {
       console.error("There was a problem with the fetch operation:", error);
     }
   };
-const rankeduserList = rankedusers.map((user) => (
-    <li key={user.rank}>
-        <Rankeduser name={user.name} tasks={user.tasks}></Rankeduser>
+
+  let rankeduserList = rankedusers.map((user) => (
+    <li key={user.username}>
+      <Rankeduser name={user.username} tasks={user.solved}></Rankeduser>
     </li>
-));
+  ));
 
   return (
     <>
-    <div className="leaderboard-container">
-    <div className="top-10">🏆 Top 10 Coders</div>
-      <ol>
-        {rankeduserList}
-      </ol>
-    </div>
+      <div className="leaderboard-container">
+        <div className="top-10">🏆 Top 10 Coders</div>
+        <ol>
+          {rankeduserList}
+        </ol>
+      </div>
     </>
   );
 }
