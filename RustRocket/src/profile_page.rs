10b8c0e68683a885473232d96a931ae8614f81b8
imbrsk <boris696boris@gmail.com>
2024-05-09 17:50:22 +0200

    refactor: Improve error handling in ChangeUsername endpoint
    The ChangeUsername endpoint in profile_page.rs has been refactored to improve error handling. Instead of returning a generic "Username in use" error message, it now returns a more specific error message "Username is taken". Similarly, the success response now includes the "response" field with the value "Username changed successfully". This change enhances the clarity and user experience of the endpoint.

--- RustRocket/src/profile_page.rs
+++ RustRocket/src/profile_page.rs
@@ -103,8 +103,8 @@ impl ChangeUsername{
         let user_id = ProfilePage::get_user_id(self.session.clone(),pool).await;
         let response = ChangeUsername::set_new_username(self, user_id, pool).await;
         match response {
-            Ok(_) => return Json(json!({"status": "Username changed successfully"})),
-            Err(_) => return Json(json!({"status": "Username in use"})),
+            Ok(_) => return Json(json!({"status": "sucsess", "response": "Username changed successfully"})),
+            Err(_) => return Json(json!({"status": "error", "response": "Username is taken"})),
         }
     }
 }
